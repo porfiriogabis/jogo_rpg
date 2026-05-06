f8ecf3c10b9e0128d12a2a1b159c5c01f26b7322
porfiriogabis <gabrielaporfiriosilvagrc@gmail.com>
2026-05-06 15:55:29 -0300

    função incorreta
    Erro na hora de chamar aas funçoes

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,12 +1,12 @@
 print("Você foi capturado por um goblin do mal...")
-print("Saia da cela e ache um jeito de escapar!")
+print("Saia da cela e ache um jeito de escapar do castelo!")
 
 def nome_classe():
     print("Escolha sua classe:")
     print("1 - Mago")
     print("2 - Elfa")
     print("3 - Samurai")
-          
+
     while True:
         classe = input("digite o número da classe")
 
@@ -22,13 +22,24 @@ def nome_classe():
 classe_selecionada = nome_classe()
 
 def caminho_mago():
- print(f"\nVocê escolheu a classe {classe_selecionada}")
- print("\nO que voce faz?")
- print("1 - olhar em volta e analisar o ambiente")
- print("2 - gritar como se estivesse agonizando de dor")
- print("3 - lançar um feitiço de fogo no goblin que está dormindo")
+    print(f"\nVocê escolheu a classe mago")
+    print("\nO que voce faz?")
+    print("1 - olhar em volta e analisar o ambiente")
+    print("2 - gritar como se estivesse agonizando de dor")
+    print("3 - lançar um feitiço de fogo no goblin que está dormindo")
 
 escolha_mago = input("digite o número da opção")
 
 if escolha_mago == "1":
     print("\nVocê viu uma chave pendurada no pescoço do goblin que está dormindo.")
+elif escolha_mago == "2":
+    print("\nO goblin acorda e te ataca, você morre... Fim de jogo.")
+elif escolha_mago == "3":
+    print("\nO goblin acorda assustado e te ataca, você morre... Fim de jogo.")
+else:
+    print("ops, escolha invalida, escolha uma das opçoes acima")
+
+classe_selecionada = nome_classe()
+
+if classe_selecionada == "Mago":
+        caminho_mago()
