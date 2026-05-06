c82ac1075d035f9bc0251e6ac74c36f11d3220cc
porfiriogabis <gabrielaporfiriosilvagrc@gmail.com>
2026-05-06 16:24:31 -0300

    Update Untitled-2.py
    As funções que criamos foram colocadas na posição corretas e agora estão sendo chamadas na ordem certa.

--- Untitled-2.py
+++ Untitled-2.py
@@ -2,13 +2,13 @@ print("Você foi capturado por um goblin do mal...")
 print("Saia da cela e ache um jeito de escapar do castelo!")
 
 def nome_classe():
-    print("Escolha sua classe:")
+    print("\nEscolha sua classe:")
     print("1 - Mago")
     print("2 - Elfa")
     print("3 - Samurai")
 
     while True:
-        classe = input("digite o número da classe")
+        classe = input("Digite o número da classe: ")
 
         if classe == "1":
             return "Mago"
@@ -17,29 +17,47 @@ def nome_classe():
         elif classe == "3":
             return "Samurai"
         else:
-            print("ops, opção invalida, escolha uma das opçoes acima")
-
-classe_selecionada = nome_classe()
+            print("Opção inválida, tente novamente.")
 
 def caminho_mago():
-    print(f"\nVocê escolheu a classe mago")
-    print("\nO que voce faz?")
-    print("1 - olhar em volta e analisar o ambiente")
-    print("2 - gritar como se estivesse agonizando de dor")
-    print("3 - lançar um feitiço de fogo no goblin que está dormindo")
+    print("\nVocê escolheu a classe Mago")
+    print("\nO que você faz?")
+    print("1 - Olhar em volta e analisar o ambiente")
+    print("2 - Gritar como se estivesse agonizando de dor")
+    print("3 - Lançar um feitiço de fogo no goblin que está dormindo")
 
-escolha_mago = input("digite o número da opção")
+    escolha = input("Digite o número da opção: ")
 
-if escolha_mago == "1":
-    print("\nVocê viu uma chave pendurada no pescoço do goblin que está dormindo.")
-elif escolha_mago == "2":
-    print("\nO goblin acorda e te ataca, você morre... Fim de jogo.")
-elif escolha_mago == "3":
-    print("\nO goblin acorda assustado e te ataca, você morre... Fim de jogo.")
-else:
-    print("ops, escolha invalida, escolha uma das opçoes acima")
+    if escolha == "1":
+        print("\nVocê observou que tem uma chave pendurada no pescoço do guarda goblin que está dormindo.")
+        
+        print("\nO que você faz agora?")
+        print("1 - Usar magia de nível 1")
+        print("2 - Usar magia de nível 2")
+        print("3 - Usar magia de nível 3")
+        escolha2 = input("Digite o número da opção: ")
+
+        if escolha2 == "1":
+            print("\nVocê usou uma magia semelhante a telecinese e executou na chave que trouxe até você flutuando sem fazer barulho, você abre sua cela e recupera seu livro de magia(buff), o que te permite teleportar pra fora do castelo")
+            print("Você conseguiu concluir o objetivo.🎉 VOCÊ ESCAPOU!")
+        elif escolha2 == "2":
+            print("\nA porta faz barulho...")
+            print("O goblin acorda e te mata. Fim de jogo.")
+        else:
+            print("Opção inválida.")
+            
+    elif escolha == "2":
+        print("\nO goblin acorda e te ataca... Fim de jogo.")
+    elif escolha == "3":
+        print("\nO goblin acorda assustado e te mata... Fim de jogo.")
+    else:
+        print("Opção inválida.")
 
 classe_selecionada = nome_classe()
 
 if classe_selecionada == "Mago":
-        caminho_mago()
+    caminho_mago()
+elif classe_selecionada == "Elfa":
+    print("\nCaminho da Elfa ainda não foi criado...")
+elif classe_selecionada == "Samurai":
+    print("\nCaminho do Samurai ainda não foi criado...")
